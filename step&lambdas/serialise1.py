2234efea17a13810c2fd0c0f167b1b880a9a5473
Salsabeel Tantoush <60573989+salsabeel-tn@users.noreply.github.com>
2021-12-15 15:39:12 +0300

    Add files via upload

---
+++ step&lambdas/serialise1.py
@@ -0,0 +1,31 @@
+import json
+import boto3
+import base64
+
+s3 = boto3.client('s3')
+
+def lambda_handler(event, context):
+    """A function to serialize target data from S3"""
+
+    # Get the s3 address from the Step Function event input
+    key = event["s3_key"]
+    bucket = "sagemaker-studio-fs9x3ynpsv7"
+   
+
+    # Download the data from s3 to /tmp/image.png
+    boto3.resource('s3').Bucket(bucket).download_file(key, '/tmp/image.png')
+    # We read the data from a file
+    with open("/tmp/image.png", "rb") as f:
+        image_data = base64.b64encode(f.read())
+
+    # Pass the data back to the Step Function
+    print("Event:", event.keys())
+    return {
+        'statusCode': 200,
+        'body': {
+            "image_data": image_data,
+            "s3_bucket": bucket,
+            "s3_key": key,
+            "inferences": []
+        }
+    }
